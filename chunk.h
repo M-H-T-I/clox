4e65432769cd9a9cec3faf81ed6d96e06a384aed
M-H-T-I <hassantariqirshad@gmail.com>
2025-10-11 08:15:32 +0500

    implementing logic to store line numbers in code repreentaion as well

--- chunk.h
+++ chunk.h
@@ -6,11 +6,13 @@
 #include "common.h"
 #include "value.h"
 
+
 // all possible bytecode instructions like the ones in assembly
 typedef enum{
     OP_CONSTANT, // when this instruction is excecuted the vm loads the constant for use.
     OP_RETURN, // return value from current function
 } OpCode; 
+
 
 
 // represents a block of compiled bytecode (a function, method, block, script, etc.)
@@ -21,13 +23,13 @@ typedef struct {
 
     // a pointer to a bytecode opcode
     uint8_t* code;
-
-    ValueArray constants;
+    int* lines;
+    ValueArray constants; // stores constant values
 
 }Chunk;
 
 void initChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
 void freeChunk(Chunk* chunk);
 int addConstant(Chunk* chunk, Value value); // an explicit function used to add values to the contant's values array. Calls writeValueArray and returns the index where value was stored
 
