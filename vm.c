9a6bf19d53410414f548fa48c17ef66e972bab22
M-H-T-I <hassantariqirshad@gmail.com>
2025-10-16 19:01:00 +0500

    creat run function,  and interpret functiom

--- vm.c
+++ vm.c
@@ -11,3 +11,27 @@ void initVm(){
 void freeVm(){
 
 }
+
+InterpretResult interpret(Chunk* chunk){
+    vm.chunk = chunk; // both are pointers 
+    vm.ip = vm.chunk->code;
+    return run();
+}
+
+static InterpretResult run(){
+
+    #define READ_BYTE() (*vm.ip++)
+
+    for(;;){
+        uint8_t instruction;
+
+        switch (instruction = READ_BYTE())
+        {
+        case OP_RETURN:
+            return INTERPRET_OK;
+            break;
+        }
+    }
+
+    #undef READ_BYTE
+}
--- vm.h
+++ vm.h
@@ -7,11 +7,25 @@
 typedef struct {
 
     Chunk* chunk;
+    
+    /* (instruction pointer) keeps track of which intsruction will be excecuted next (program counter)
+    (in code array in chunk) */
+
+    uint8_t* ip;
 
 }VM;
 
+typedef enum{
+
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
+
+}InterpretResult;
+
 void initVM();
 void freeVM();
+InterpretResult interpret(Chunk* chunk);
 
 
 #endif
