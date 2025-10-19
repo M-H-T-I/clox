81e4916ef5889817381c62c3c6dbc9d7381e75d4
M-H-T-I <hassantariqirshad@gmail.com>
2025-10-19 11:14:12 +0500

    Create scanner.h, scanner.c with declaration for initScanner and the typdef for Scanner struct

---
+++ scanner.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "common.h"
+#include "scanner.h"
+
+
+typedef struct{
+
+    const char* start;
+    const char* current;
+    int line;
+
+}Scanner;
+
+Scanner scanner;
